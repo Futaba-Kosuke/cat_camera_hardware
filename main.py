6fdd99278e18d793297002f10a8978487b0d65cc
Futaba-Kosuke <k.futaba2828@gmail.com>
2020-09-27 14:13:49 +0900

    Add: firebase_storageへのアップロードを実装 (#2)

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import sys
 import time
 from threading import Thread
 import importlib.util
+import firebase_admin
+from firebase_admin import credentials, firestore, storage
 
 class VideoStream:
     
@@ -38,105 +40,128 @@ class VideoStream:
     def stop(self):
         self.stopped = True
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--modeldir', help='model\'s path', required=True)
-parser.add_argument('--display', help='flag\'s window', default=True)
+class Firebase:
 
-args = parser.parse_args()
+    def __init__(self, cred_path='./firebase/cred.json', bucket_name='cat-camera-cafe8.appspot.com'):
+        cred = credentials.Certificate(cred_path)
+        firebase_admin.initialize_app(cred, {'storageBucket': bucket_name})
 
-MODEL_NAME = args.modeldir
-IS_DISPLAY = args.display
-GRAPH_NAME = 'detect.tflite'
-LABELMAP_NAME = 'labelmap.txt'
-min_conf_threshold = float(0.5)
-imgW, imgH = 1280, 720
+        self.bucket = storage.bucket()
 
-pkg = importlib.util.find_spec('tflite_runtime')
-if pkg:
-    from tflite_runtime.interpreter import Interpreter
-    print('use tflite')
+    def upload_file(self, file_path='./firebase/image.jpeg', content_type='image/jpeg'):
+        blob = self.bucket.blob(file_path.split('/')[-1])
+        with open(file_path, 'rb') as f:
+            blob.upload_from_file(f, content_type=content_type)
 
-else:
-    from tensorflow.lite.python.interpreter import Interpreter
-    print('use tf')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--modeldir', help='model\'s path', default='models')
+    parser.add_argument('--display', help='flag\'s window', default=True)
+    parser.add_argument('--test', help='is test', default=True)
 
-CWD_PATH = os.getcwd()
-PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, GRAPH_NAME)
-PATH_TO_LABELS = os.path.join(CWD_PATH, MODEL_NAME, LABELMAP_NAME)
+    args = parser.parse_args()
+    
+    MODEL_NAME = args.modeldir
+    IS_DISPLAY = args.display
+    IS_TEST = args.test
+    GRAPH_NAME = 'detect.tflite'
+    LABELMAP_NAME = 'labelmap.txt'
+    min_conf_threshold = float(0.5)
+    imgW, imgH = 1280, 720
+    
+    pkg = importlib.util.find_spec('tflite_runtime')
+    if pkg:
+        from tflite_runtime.interpreter import Interpreter
+        print('use tflite')
+    
+    else:
+        from tensorflow.lite.python.interpreter import Interpreter
+        print('use tf')
+    
+    CWD_PATH = os.getcwd()
+    PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, GRAPH_NAME)
+    PATH_TO_LABELS = os.path.join(CWD_PATH, MODEL_NAME, LABELMAP_NAME)
+    
+    with open(PATH_TO_LABELS, 'r') as f:
+        labels = [line.strip() for line in f.readlines()]
+    
+    if labels[0] == '???':
+        del(labels[0])
+    
+    labels = np.array(labels)
+    
+    interpreter = Interpreter(model_path=PATH_TO_CKPT)
+    
+    interpreter.allocate_tensors()
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    height = input_details[0]['shape'][1]
+    width = input_details[0]['shape'][2]
+    
+    floating_model = (input_details[0]['dtype'] == np.float32)
+    
+    input_mean = 127.5
+    input_std = 127.5
+    
+    frame_rate_calc = 1
+    freq = cv2.getTickFrequency()
+    
+    videostream = VideoStream(resolution=(imgW, imgH), framerate=30).start()
+    time.sleep(1)
 
-with open(PATH_TO_LABELS, 'r') as f:
-    labels = [line.strip() for line in f.readlines()]
-
-if labels[0] == '???':
-    del(labels[0])
-
-labels = np.array(labels)
-
-interpreter = Interpreter(model_path=PATH_TO_CKPT)
-
-interpreter.allocate_tensors()
-input_details = interpreter.get_input_details()
-output_details = interpreter.get_output_details()
-height = input_details[0]['shape'][1]
-width = input_details[0]['shape'][2]
-
-floating_model = (input_details[0]['dtype'] == np.float32)
-
-input_mean = 127.5
-input_std = 127.5
-
-frame_rate_calc = 1
-freq = cv2.getTickFrequency()
-
-videostream = VideoStream(resolution=(imgW, imgH), framerate=30).start()
-time.sleep(1)
-
-while True:
-    t1 = cv2.getTickCount()
-
-    frame = videostream.read()
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame_resized = cv2.resize(frame_rgb, (width, height))
-    input_data = np.expand_dims(frame_resized, axis=0)
-
-    if floating_model:
-        input_data = (np.float32(input_data) - input_mean) / input_std
-
-    interpreter.set_tensor(input_details[0]['index'], input_data)
-    interpreter.invoke()
-
-    boxes = interpreter.get_tensor(output_details[0]['index'])[0]
-    classes = interpreter.get_tensor(output_details[1]['index'])[0]
-    scores = interpreter.get_tensor(output_details[2]['index'])[0]
-
-    labels_valid = labels[np.int64(classes[(scores > min_conf_threshold) & (scores <= 1.0)])]
-    if 'cat' in labels_valid and 'person' in labels_valid:
-        print('cat_and_person')
-
-    if IS_DISPLAY:
-        for i in range(len(scores)):
-            if (scores[i] > min_conf_threshold) and (scores[i] <= 1.0):
-                x_min, x_max = int(max(1, (boxes[i][1] * imgW))), int(min(imgW, (boxes[i][3] * imgW)))
-                y_min, y_max = int(max(1, (boxes[i][0] * imgH))), int(min(imgH, (boxes[i][2] * imgH)))
-
-                cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (10, 255, 0), 2)
-
-                object_name = labels[int(classes[i])]
-                label = '%s: %d%%' % (object_name, int(scores[i]*100))
-                labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
-                label_ymin = max(y_min, labelSize[1] + 10)
-                cv2.rectangle(frame, (x_min, label_ymin-labelSize[1]-10), (x_min+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED)
-                cv2.putText(frame, label, (x_min, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
-
-        cv2.putText(frame, 'FPS: {0:.2f}'.format(frame_rate_calc), (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
-        cv2.imshow('Object detector', frame)
-
-        t2 = cv2.getTickCount()
-        time1 = (t2 - t1) / freq
-        frame_rate_calc = 1 / time1
-
-        if cv2.waitKey(1) == ord('q'):
-            break
-
-cv2.destroyAllWindows()
-videostream.stop()
+    firebase = Firebase()
+    upload_flag = True
+    
+    while True:
+        t1 = cv2.getTickCount()
+    
+        frame = videostream.read()
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_resized = cv2.resize(frame_rgb, (width, height))
+        input_data = np.expand_dims(frame_resized, axis=0)
+    
+        if floating_model:
+            input_data = (np.float32(input_data) - input_mean) / input_std
+    
+        interpreter.set_tensor(input_details[0]['index'], input_data)
+        interpreter.invoke()
+    
+        boxes = interpreter.get_tensor(output_details[0]['index'])[0]
+        classes = interpreter.get_tensor(output_details[1]['index'])[0]
+        scores = interpreter.get_tensor(output_details[2]['index'])[0]
+    
+        labels_valid = labels[np.int64(classes[(scores > min_conf_threshold) & (scores <= 1.0)])]
+        if 'cat' in labels_valid and 'person' in labels_valid and upload_flag:
+            print('cat_and_person')
+            cv2.imwrite('./firebase/image.jpeg', frame)
+            firebase.upload_file()
+            if IS_TEST:
+                upload_flag = False
+    
+        if IS_DISPLAY:
+            for i in range(len(scores)):
+                if (scores[i] > min_conf_threshold) and (scores[i] <= 1.0):
+                    x_min, x_max = int(max(1, (boxes[i][1] * imgW))), int(min(imgW, (boxes[i][3] * imgW)))
+                    y_min, y_max = int(max(1, (boxes[i][0] * imgH))), int(min(imgH, (boxes[i][2] * imgH)))
+    
+                    cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (10, 255, 0), 2)
+    
+                    object_name = labels[int(classes[i])]
+                    label = '%s: %d%%' % (object_name, int(scores[i]*100))
+                    labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
+                    label_ymin = max(y_min, labelSize[1] + 10)
+                    cv2.rectangle(frame, (x_min, label_ymin-labelSize[1]-10), (x_min+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED)
+                    cv2.putText(frame, label, (x_min, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
+    
+            cv2.putText(frame, 'FPS: {0:.2f}'.format(frame_rate_calc), (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
+            cv2.imshow('Object detector', frame)
+    
+            t2 = cv2.getTickCount()
+            time1 = (t2 - t1) / freq
+            frame_rate_calc = 1 / time1
+    
+            if cv2.waitKey(1) == ord('q'):
+                break
+    
+    cv2.destroyAllWindows()
+    videostream.stop()
